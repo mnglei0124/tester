dda06a6f48d594ab642cd6aab01c1bdce9091eea
mungunkhishig.t <mungunkhishig.t@corp.unitel.mn>
2026-03-27 15:32:25 +0800

    origin fix

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  serverExternalPackages: ["pg"],
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["192.168.250.251", "192.168.250.251:3000"],
+    },
+  },
 };
 
 export default nextConfig;
